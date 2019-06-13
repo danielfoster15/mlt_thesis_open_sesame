b376dc3ae14e4a13533f3f8f5b24b967022590d1
foster <foster@textkernel.nl>
2019-06-13 15:31:15 +0200

    data exploration script added

---
+++ explore_mistakes.py
@@ -0,0 +1,33 @@
+from collections import Counter
+model = 'argid_no_data_fes'
+gold_standard = 'fn1.7.dev.syntaxnet_no_data_fes.conll'
+
+
+with open('logs/'+model+'/predicted-1.7-argid-dev.conll', 'r') as f:
+	predicted_FEs =[]
+	for line in f:
+		if line != '\n':
+			predicted_FE = line.split()[-1]
+			predicted_FEs.append(predicted_FE)
+
+with open('data/neural/fn1.7/'+gold_standard, 'r') as f:
+	gold_FEs =[]
+	for line in f:
+		if line != '\n':
+			gold_FE = line.split()[-1]
+			gold_FEs.append(gold_FE)
+
+recall_problems = []
+precision_problems = []
+for gold, prediction in list(zip(gold_FEs, predicted_FEs)):
+	if gold != prediction:
+		#print('gold: ',gold,' prediction: ', prediction)
+		if prediction == 'O':
+			recall_problems.append(gold)
+		else:
+			precision_problems.append(gold)
+
+
+
+print(Counter(recall_problems).most_common())
+print(Counter(precision_problems).most_common())
